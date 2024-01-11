93e06ae3693fd45f5b743576f827aabd5ba1db25
hosung-222 <gik11kr@naver.com>
2024-01-11 21:45:13 +0900

    :sparkles: feat: SecurityConfig 기본 설정 구현
    csrf : disable, fromLogin : disable, httpBasic : disable, 경로별 인가 작업, 세션 : STATELESS

---
+++ src/main/java/com/security/springjwt/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.security.springjwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        // csrf disable 설정
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+        // Form 로그인 방식 disable
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        // http basic 인증 방식 disable
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        // 경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated() // 그 외 나머지 요청에 대해 로그인 사용자 접근 가능
+                );
+
+        // 세션 STATELESS 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
